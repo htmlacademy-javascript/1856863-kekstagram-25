957c01d959c395580fcb2e3eff0bf627489feb25
Diana <demonshiar@gmail.com>
2022-02-28 12:51:08 +0300

    Исправления

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 // генерация случайного числа, включается максимум и минимум
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-function getRandomIntInclusive(min, max) {
+function getRandom (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+getRandom (0, 12);
+
 // проверка максимальной длинны строки
 // https://www.w3resource.com/javascript/form/string-length.php
 
-function lengthRange(inputtxt, minlength, maxlength) {
-  const userInput = inputtxt.value;
-   if(userInput.length >= minlength && userInput.length <= maxlength)
-      {
-        return true;
-      }
-   else
-      {
-        return false;
-      }
+function getRange (text, maxlength) {
+  if(text.lenght <= maxlength) {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
+
+getRange(1, 120);
