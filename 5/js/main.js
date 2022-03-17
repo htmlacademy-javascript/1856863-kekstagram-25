7b895360518f776f7771ede093f6160a1687a598
Keks <keks@htmlacademy.ru>
2022-03-17 11:46:42 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,6 @@
+import {getOutput} from './data.js';
+import {getRandomNumber, getRange} from './util.js';
+
+getOutput();
+getRandomNumber(1, 2);
+getRange('Check', 10);
---
+++ 5/js/util.js
@@ -0,0 +1,48 @@
+// Генерация случайного числа, включается максимум и минимум
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+const getRandomNumber = (min, max) => {
+  min = Math.ceil(min);
+  if(min < 0) {
+    min = Math.abs(min);
+  }
+  max = Math.floor(max);
+  if(max < 0) {
+    max = Math.abs(max);
+  }
+  if (max <= min ) {
+    max = min;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+// Проверка максимальной длинны строки
+
+const getRange = (stringChecked, maxLength) => stringChecked.length <= maxLength;
+
+getRange(1, 120);
+
+// Генерация случайного числа из массива
+
+let RandomNumberArray;
+const getRandomNumberArray = (min,max) => {
+  const array = [];
+  for (let i = min; i < max; i++) {
+    RandomNumberArray = getRandomNumber(min, max);
+    if (array.includes(RandomNumberArray) === true) {
+      i = i - 1;
+    } else {
+      if (RandomNumberArray > max === false) {
+        array.push(RandomNumberArray);
+      }
+    }
+  }
+  return RandomNumberArray;
+};
+
+export {getRandomNumber};
+export {getRange};
+export {getRandomNumberArray};
+
+
